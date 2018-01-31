523c05daf5dae20148ad9c5da57e5b2f4ef0a5a1
thiago <thiagoprocaci@gmail.com>
2018-01-30 23:26:01 -0200

    degree vector for structure similarity

--- src/main/java/com/tbp/network/sample/Main.java
+++ src/main/java/com/tbp/network/sample/Main.java
@@ -5,23 +5,59 @@ import com.tbp.network.gui.GuiBuilder;
 import com.tbp.network.model.BarabasiModel;
 import com.tbp.network.model.ErdosRenyi;
 import com.tbp.network.model.NetworkModel;
+import com.tbp.network.structure.DegreeSequence;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
 
 import javax.swing.*;
+import java.util.List;
+import java.util.Map;
 
 
 public class Main {
     public static void main(String args[]) {
-
-        
-        //          new ErdosRenyi(1000);
         
         //System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         // new ErdosRenyi(10000);//
-        NetworkModel model =  new BarabasiModel(10000);
+        //NetworkModel model =  new BarabasiModel(10000);
 
-        model.getGraph().display();
+        //model.getGraph().display();
+
+        Graph g = new SingleGraph("Graph");
+
+
+        g.addNode("A");
+        g.addNode("B");
+        g.addNode("C");
+        g.addNode("U");
+        g.addNode("V");
+        g.addNode("D");
+        g.addNode("E");
+        g.addNode("F");
+        g.addNode("G");
+
+
+        g.addEdge("A_B", "A", "B");
+        g.addEdge("A_C", "A", "C");
+        g.addEdge("C_B", "C", "B");
+        g.addEdge("B_V", "B", "V");
+        g.addEdge("B_U", "B", "U");
+        g.addEdge("U_V", "U", "V");
+        g.addEdge("U_E", "U", "E");
+        g.addEdge("U_D", "U", "D");
+        g.addEdge("V_D", "V", "D");
+        g.addEdge("D_F", "D", "F");
+        g.addEdge("D_G", "D", "G");
+        g.addEdge("F_G", "F", "G");
+
+        DegreeSequence degreeSequence = new DegreeSequence();
+
+        Map<Integer, List<Integer>> u = degreeSequence.execute("U", g);
+        System.out.println(u);
+        u = degreeSequence.execute("V", g);
+        System.out.println(u);
 
     }
 
---
+++ src/main/java/com/tbp/network/structure/DegreeSequence.java
@@ -0,0 +1,71 @@
+package com.tbp.network.structure;
+
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+import java.util.*;
+
+public class DegreeSequence {
+
+    public static final String VISITED = "visited";
+    public static final String LEVEL = "LEVEL";
+
+
+    public Map<Integer, List<Integer>> execute(String startNode, Graph graph) {
+
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+
+        Node root = graph.getNode(startNode);
+        root.setAttribute(VISITED, true);
+        root.setAttribute(LEVEL, 0);
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            Node node = queue.remove();
+            Integer currentLevel = node.getAttribute(LEVEL, Integer.class);
+            if(map.get(currentLevel) == null) {
+                map.put(currentLevel, new ArrayList<Integer>());
+            }
+            map.get(currentLevel).add(node.getDegree());
+            Node child;
+            while((child = getUnvisitedChildNode(node)) != null) {
+                child.setAttribute(VISITED, true);
+                child.setAttribute(LEVEL, node.getAttribute(LEVEL, Integer.class) + 1);
+                queue.add(child);
+            }
+        }
+        cleanVisited(graph);
+        sortList(map);
+        return map;
+    }
+
+    private void sortList(Map<Integer, List<Integer>> map) {
+        for(Integer key: map.keySet()) {
+            List<Integer> list = map.get(key);
+            Collections.sort(list);
+            map.put(key, list);
+        }
+    }
+
+    private void cleanVisited(Graph graph) {
+        Iterable<? extends Node> eachNode = graph.getEachNode();
+        for(Node n: eachNode) {
+            n.setAttribute(VISITED, false);
+        }
+    }
+
+    private Node getUnvisitedChildNode(Node node) {
+        Iterator<Node> neighborNodeIterator = node.getNeighborNodeIterator();
+        while(neighborNodeIterator.hasNext()) {
+            Node next = neighborNodeIterator.next();
+            Boolean visited = next.getAttribute(VISITED, Boolean.class);
+            if(visited == null || !visited) {
+                return next;
+            }
+        }
+        return null;
+    }
+
+
+}
